5fd4944a4dcab1e617dbef2015b5c7888f89760b
Almeida Júnior, Luiz - Jornada Ed. Tech <luiz@jornadatecnologia.com>
2025-04-22 01:36:28 -0300

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ParticipantModule } from './participants/participant.module';
-
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/karaoke'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI || '', {
+      user: process.env.USER_DB,
+      pass: process.env.PASS_DB,
+      authSource: process.env.AUTH_SOURCE_DB,
+    }),
     ParticipantModule,
   ],
 })
